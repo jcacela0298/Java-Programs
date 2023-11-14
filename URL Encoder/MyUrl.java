f40a6a01a7bb06c1fff3c6a5cb11b144f42c2372
jcacela0298 <132624484+jcacela0298@users.noreply.github.com>
2023-11-14 14:28:07 -0500

    Update MyUrl.java

--- URL Encoder/MyUrl.java
+++ URL Encoder/MyUrl.java
@@ -52,7 +52,8 @@ public class MyUrl {
 		
 		public static String urlEncode(String text) {
 		String modifiedQuote = new String("");
-	       String normalChars = new String ("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,0,1,2,3,4,5,6,7,8,9,_,-,.,*");
+	        String normalChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.*";
+
 	            for (int n = 0; n < text.length(); ++n) {
 	                char currentCharacter = text.charAt(n);
 	                    if (currentCharacter == ' ') {
