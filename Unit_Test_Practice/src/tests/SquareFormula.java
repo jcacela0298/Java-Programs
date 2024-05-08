d1ef1bde25ec8cd4c752eb6893247d00f0cf20d6
Jack Cacela <jackcacela5@gmail.com>
2024-05-08 12:34:23 -0400

    Adding JUnit Testing Example

---
+++ Unit_Test_Practice/src/tests/SquareFormula.java
@@ -0,0 +1,20 @@
+package tests;
+
+public class SquareFormula {
+
+    public static int areaOfSquare(int number) {
+
+        return number * number;
+
+    }
+
+
+public static void main(String[] args) {
+
+    int square1 = areaOfSquare(7);
+
+    System.out.println("Maximum value: " + square1);
+
+}
+
+}
---
+++ Unit_Test_Practice/src/tests/SquareFormulaTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SquareFormulaTest {
+
+    @Test
+    public void testAreaOfSquare() {
+
+        // Arrange
+
+        int sideLength = 5;
+
+        int expectedArea = 25;
+
+
+        // Act
+
+        int actualArea = SquareFormula.areaOfSquare(sideLength);
+
+
+        // Assert
+
+        assertEquals(expectedArea, actualArea);
+
+    }
+
+    
+    @Test
+    public void testAreaOfSquareWithSideLength16() {
+
+        // Arrange
+
+        int sideLength = 16;
+
+        int expectedArea = 256;
+
+
+        // Act
+
+        int actualArea = SquareFormula.areaOfSquare(sideLength);
+
+
+        // Assert
+
+        assertEquals(expectedArea, actualArea);
+
+    }
+
+
+}
